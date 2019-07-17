43af477a233171b49707c183ec73fda6732c5de9
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-07-17 08:35:10 -0400

    Updating project structure and build configuration.

--- src/AutomationFoundation.Hosting.Abstractions/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-#if DEBUG
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-[assembly: InternalsVisibleTo("AutomationFoundation.Hosting.Abstractions.Tests")]
-#endif
