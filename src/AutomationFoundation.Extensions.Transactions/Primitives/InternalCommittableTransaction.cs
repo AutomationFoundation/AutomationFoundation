d5f063537895d5544b7d1f0b23003f701e39d5d4
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-08-24 08:21:38 -0400

    Adding extensions for System.Transactions assembly.

---
+++ src/AutomationFoundation.Extensions.Transactions/Primitives/InternalCommittableTransaction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Transactions;
+
+namespace AutomationFoundation.Extensions.Transactions.Primitives
+{
+    internal class InternalCommittableTransaction : IDisposable
+    {
+        public virtual CommittableTransaction UnderlyingTransaction { get; }
+
+        public InternalCommittableTransaction()
+        {
+        }
+
+        public InternalCommittableTransaction(CommittableTransaction transaction)
+        {
+            UnderlyingTransaction = transaction ?? throw new ArgumentNullException(nameof(transaction));
+        }
+
+        ~InternalCommittableTransaction()
+        {
+            Dispose(false);
+        }
+
+        public virtual void Commit()
+        {
+            UnderlyingTransaction.Commit();
+        }
+
+        public virtual void Rollback()
+        {
+            UnderlyingTransaction.Rollback();
+        }
+
+        public virtual void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                UnderlyingTransaction.Dispose();
+            }
+        }
+    }
+}
---
+++ test/AutomationFoundation.Extensions.Transactions.Tests/CommittableTransactionAdapterTests.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Transactions;
+using AutomationFoundation.Extensions.Transactions.Primitives;
+using Moq;
+using NUnit.Framework;
+
+namespace AutomationFoundation.Extensions.Transactions
+{
+    public class CommittableTransactionAdapterTests
+    {
+        private Mock<InternalCommittableTransaction> transaction;
+
+        [SetUp]
+        public void Setup()
+        {
+            transaction = new Mock<InternalCommittableTransaction>();
+        }
+
+        [Test]
+        public void MustCommitTheTransaction()
+        {
+            using (var target = new CommittableTransactionAdapter(transaction.Object))
+            {
+                target.Commit();
+            }
+
+            transaction.Verify(o => o.Commit(), Times.Once);
+        }
+
+        [Test]
+        public void MustRollbackTheTransaction()
+        {
+            using (var target = new CommittableTransactionAdapter(transaction.Object))
+            {
+                target.Rollback();
+            }
+
+            transaction.Verify(o => o.Rollback(), Times.Once);
+        }
+
+        [Test]
+        public void MustDisposeTheTransactionWhenOwned()
+        {
+            using (var target = new CommittableTransactionAdapter(transaction.Object))
+            {
+                Assert.True(target.OwnsTransaction);
+            }
+
+            transaction.Verify(o => o.Dispose(), Times.Once);
+        }
+
+        [Test]
+        public void MustNotDisposeTheTransactionWhenNotOwned()
+        {
+            using (var target = new CommittableTransactionAdapter(transaction.Object, false))
+            {
+                Assert.False(target.OwnsTransaction);
+            }
+
+            transaction.Verify(o => o.Dispose(), Times.Never);
+        }
+
+        [Test]
+        public void ThrowsExceptionWhenAlreadyDisposedOnCommit()
+        {
+            var target = new CommittableTransactionAdapter(transaction.Object);
+            target.Dispose();
+
+            Assert.Throws<ObjectDisposedException>(() => target.Commit());
+        }
+
+        [Test]
+        public void ThrowsExceptionWhenAlreadyDisposedOnRollback()
+        {
+            var target = new CommittableTransactionAdapter(transaction.Object);
+            target.Dispose();
+
+            Assert.Throws<ObjectDisposedException>(() => target.Rollback());
+        }
+
+        [Test]
+        public void ReturnsTheUnderlyingTransactionAsExpected()
+        {
+            var expected = new CommittableTransaction();
+            transaction.Setup(o => o.UnderlyingTransaction).Returns(expected);
+
+            using (var target = new CommittableTransactionAdapter(transaction.Object))
+            {
+                Assert.AreEqual(expected, target.UnderlyingTransaction);
+            }
+        }
+
+        [Test]
+        public void ReturnsTheUnderlyingTransactionAsExpectedWhenUsingRealObject()
+        {
+            var expected = new CommittableTransaction();
+
+            using (var target = new CommittableTransactionAdapter(expected))
+            {
+                Assert.AreEqual(expected, target.UnderlyingTransaction);
+            }
+        }
+
+        [Test]
+        public void ThrowsAnExceptionWhenTheTransactionIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CommittableTransactionAdapter((CommittableTransaction)null));
+        }
+    }
+}
