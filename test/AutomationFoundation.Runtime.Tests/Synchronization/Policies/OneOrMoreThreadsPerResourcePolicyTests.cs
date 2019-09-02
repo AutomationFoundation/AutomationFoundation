a73a9e2133277cf8d0dd5495ca3a105d25c99307
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-09-02 16:10:45 -0400

    Fixing tests.

--- test/AutomationFoundation.Runtime.Tests/Synchronization/Policies/OneOrMoreThreadsPerResourcePolicyTests.cs
+++ test/AutomationFoundation.Runtime.Tests/Synchronization/Policies/OneOrMoreThreadsPerResourcePolicyTests.cs
@@ -8,7 +8,6 @@ namespace AutomationFoundation.Runtime.Synchronization.Policies
     [TestFixture]
     public class OneOrMoreThreadsPerResourcePolicyTests
     {
-        private OneOrMoreThreadsPerResourcePolicy target;
         private CancellationTokenSource cancellationSource;
 
         [SetUp]
@@ -20,7 +19,6 @@ namespace AutomationFoundation.Runtime.Synchronization.Policies
         [TearDown]
         public void Finish()
         {
-            target?.Dispose();
             cancellationSource?.Dispose();
         }
 
@@ -37,24 +35,24 @@ namespace AutomationFoundation.Runtime.Synchronization.Policies
         }
 
         [Test]
-        [Timeout(2000)]
+        [Timeout(10000)]
         public async Task PreventMultipleThreadsFromAccessingTheResource()
         {
-            target = new OneOrMoreThreadsPerResourcePolicy(1);
+            using (var target = new OneOrMoreThreadsPerResourcePolicy(1))
+            {
+                await target.AcquireLockAsync(CancellationToken.None);
 
-            await target.AcquireLockAsync(CancellationToken.None);
+                cancellationSource.CancelAfter(1000);
 
-            cancellationSource.CancelAfter(1000);
-
-            Assert.ThrowsAsync<TaskCanceledException>(async () => await target.AcquireLockAsync(cancellationSource.Token));
+                Assert.ThrowsAsync<OperationCanceledException>(async () => await target.AcquireLockAsync(cancellationSource.Token));
+            }
         }
 
         [Test]
         [Timeout(2000)]
         public void ThrowsAnExceptionAfterBeingDisposed()
         {
-            target = new OneOrMoreThreadsPerResourcePolicy(1);
-
+            var target = new OneOrMoreThreadsPerResourcePolicy(1);
             target.Dispose();
 
             Assert.ThrowsAsync<ObjectDisposedException>(async () => await target.AcquireLockAsync(CancellationToken.None));
