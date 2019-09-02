a763b0f2fce51ee50a237dd36d1afb5a57fe6985
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-09-02 09:34:29 -0400

    Adding unit tests.

--- test/AutomationFoundation.Runtime.Tests/AutomationRuntimeTests.cs
+++ test/AutomationFoundation.Runtime.Tests/AutomationRuntimeTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using AutomationFoundation.Runtime.Abstractions;
+using Moq;
 using NUnit.Framework;
 
 namespace AutomationFoundation.Runtime
@@ -6,6 +8,64 @@ namespace AutomationFoundation.Runtime
     [TestFixture]
     public class AutomationRuntimeTests
     {
+        private Mock<IProcessor> processor;
+
+        [SetUp]
+        public void Setup()
+        {
+            processor = new Mock<IProcessor>();
+        }
+
+        [Test]
+        public void IdentifiesTrueIfTheProcessorIsStarted()
+        {
+            processor.Setup(o => o.State).Returns(ProcessorState.Started);
+
+            using (var target = new AutomationRuntime())
+            {
+                target.Processors.Add(processor.Object);
+
+                Assert.True(target.IsActive);
+            }
+        }
+
+        [Test]
+        public void IdentifiesTrueIfTheProcessorIsBusy()
+        {
+            processor.Setup(o => o.State).Returns(ProcessorState.Busy);
+
+            using (var target = new AutomationRuntime())
+            {
+                target.Processors.Add(processor.Object);
+
+                Assert.True(target.IsActive);
+            }
+        }
+
+        [Test]
+        public void StartsTheProcessor()
+        {
+            using (var target = new AutomationRuntime())
+            {
+                target.Processors.Add(processor.Object);
+                target.Start();
+
+                processor.Verify(o => o.Start(), Times.Once);
+            }
+        }
+
+        [Test]
+        public void StopsTheProcessor()
+        {
+            using (var target = new AutomationRuntime())
+            {
+                target.Processors.Add(processor.Object);
+                target.Stop();
+
+                processor.Verify(o => o.Stop(), Times.Once);
+            }
+        }
+
         [Test]
         public void ThrowAnExceptionWhenStartedAfterDisposed()
         {
