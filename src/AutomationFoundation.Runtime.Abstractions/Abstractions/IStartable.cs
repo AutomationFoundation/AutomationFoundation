7187eee5c2c481444a09b18268e452fae7d0006d
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-09-09 21:41:13 -0400

    Removing unused interface.

--- src/AutomationFoundation.Runtime.Abstractions/Abstractions/IStartable.cs
+++ src/AutomationFoundation.Runtime.Abstractions/Abstractions/IStartable.cs
@@ -13,18 +13,4 @@ namespace AutomationFoundation.Runtime.Abstractions
         /// <returns>The asynchronous task.</returns>
         Task StartAsync();
     }
-
-    /// <summary>
-    /// Identifies an object which can be started.
-    /// </summary>
-    /// <typeparam name="TContext">The type of object which contains contextual information while the object is being started.</typeparam>
-    public interface IStartable<in TContext>
-    {
-        /// <summary>
-        /// Starts the object asynchronously.
-        /// </summary>
-        /// <param name="context">The contextual information.</param>
-        /// <returns>The asynchronous task.</returns>
-        Task StartAsync(TContext context);
-    }
 }
