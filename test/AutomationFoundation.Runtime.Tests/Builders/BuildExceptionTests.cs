2f5a9d5360ff23e5ed2d6ae59eb479bce3a5c491
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-09-19 20:09:47 -0400

    Adding unit tests.

---
+++ test/AutomationFoundation.Runtime.Tests/Builders/BuildExceptionTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace AutomationFoundation.Runtime.Builders
+{
+    [TestFixture]
+    public class BuildExceptionTests
+    {
+        [Test]
+        public void ReturnsTheMessage()
+        {
+            var expected = "An exception occurred.";
+
+            var target = new BuildException(expected);
+            Assert.AreEqual(expected, target.Message);
+        }
+
+        [Test]
+        public void ReturnsTheException()
+        {
+            var expected = new BuildException();
+
+            var target = new BuildException("An exception occurred.", expected);
+            Assert.AreEqual(expected, target.InnerException);
+        }
+    }
+}
