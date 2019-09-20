3d9968f8f41e44113c7d3dbb6a2e31c86c8fb142
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-09-19 20:07:11 -0400

    Adding missing unit tests.

---
+++ test/AutomationFoundation.Runtime.Tests/Builders/BuilderConfigurationExceptionTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace AutomationFoundation.Runtime.Builders
+{
+    [TestFixture]
+    public class BuilderConfigurationExceptionTests
+    {
+        [Test]
+        public void ReturnsTheMessage()
+        {
+            var expected = "An exception occurred.";
+
+            var target = new BuilderConfigurationException(expected);
+            Assert.AreEqual(expected, target.Message);
+        }
+
+        [Test]
+        public void ReturnsTheException()
+        {
+            var expected = new BuilderConfigurationException();
+
+            var target = new BuilderConfigurationException("An exception occurred.", expected);
+            Assert.AreEqual(expected, target.InnerException);
+        }
+    }
+}
---
+++ test/AutomationFoundation.Tests/Hosting/DefaultRuntimeHostTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace AutomationFoundation.Hosting
+{
+    [TestFixture]
+    public class DefaultRuntimeHostTests
+    {
+        [Test]
+        public void ReturnsADefaultHostBuilder()
+        {
+            var result = DefaultRuntimeHost.CreateBuilder();
+            Assert.IsInstanceOf<DefaultRuntimeHostBuilder>(result);
+        }
+    }
+}
