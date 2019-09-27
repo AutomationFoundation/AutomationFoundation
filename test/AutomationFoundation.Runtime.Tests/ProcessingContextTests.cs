15f71b622956341d64c4915c0fed883de2b9de74
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-09-27 18:22:08 -0400

    Adding unit tests.

--- test/AutomationFoundation.Runtime.Tests/ProcessingContextTests.cs
+++ test/AutomationFoundation.Runtime.Tests/ProcessingContextTests.cs
@@ -1,46 +1,75 @@
-﻿//using System;
-//using NUnit.Framework;
+﻿using System;
+using AutomationFoundation.Runtime.Stubs;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
 
-//namespace AutomationFoundation.Runtime
-//{
-//    [TestFixture]
-//    public class ProcessingContextTests
-//    {
-//        [Test]
-//        public void ReturnsTheIdCorrectly()
-//        {
-//            Guid expected = Guid.NewGuid();
-//            var target = new ProcessingContext(expected);
+namespace AutomationFoundation.Runtime
+{
+    [TestFixture]
+    public class ProcessingContextTests
+    {
+        private Guid id;
+        private Mock<IServiceScope> scope;
 
-//            Assert.AreEqual(expected, target.Id);
-//        }
+        [SetUp]
+        public void Setup()
+        {
+            id = Guid.NewGuid();
+            scope = new Mock<IServiceScope>();
+        }
 
-//        [Test]
-//        public void SetsTheCurrentContext()
-//        {
-//            var expected = new ProcessingContext(Guid.NewGuid());
-//            ProcessingContext.SetCurrent(expected);
+        [Test]
+        public void ReturnsTheIdCorrectly()
+        {
+            using (var target = new StubProcessingContext(id, scope.Object))
+            {
+                Assert.AreEqual(id, target.Id);
+            }
+        }
 
-//            var result = ProcessingContext.Current;
+        [Test]
+        public void SetsTheCurrentContext()
+        {
+            using (var expected = new StubProcessingContext(id, scope.Object))
+            {
+                ProcessingContext.SetCurrent(expected);
 
-//            Assert.AreEqual(expected, result);
-//        }
+                var result = ProcessingContext.Current;
 
-//        [Test]
-//        public void ThrowsAnExceptionWhenTheContextIsNull()
-//        {
-//            Assert.Throws<ArgumentNullException>(() => ProcessingContext.SetCurrent(null));
-//        }
+                Assert.AreEqual(expected, result);
+            }
+        }
 
-//        [Test]
-//        public void ClearsTheCurrentContext()
-//        {
-//            ProcessingContext.SetCurrent(new ProcessingContext(Guid.NewGuid()));
-//            Assert.IsNotNull(ProcessingContext.Current);
+        [Test]
+        public void ThrowsAnExceptionWhenTheContextIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => ProcessingContext.SetCurrent(null));
+        }
 
-//            ProcessingContext.Clear();
+        [Test]
+        public void ClearsTheCurrentContext()
+        {
+            using (var target = new StubProcessingContext(id, scope.Object))
+            {
+                ProcessingContext.SetCurrent(target);
+                Assert.IsNotNull(ProcessingContext.Current);
 
-//            Assert.IsNull(ProcessingContext.Current);
-//        }
-//    }
-//}
+                ProcessingContext.Clear();
+
+                Assert.IsNull(ProcessingContext.Current);
+            }
+        }
+
+        [Test]
+        public void DisposesTheScopeWhenTheContextIsDisposed()
+        {
+            using (var _ = new StubProcessingContext(id, scope.Object))
+            {
+                // This block intentionally left blank.
+            }
+
+            scope.Verify(o => o.Dispose(), Times.Once);
+        }
+    }
+}
---
+++ test/AutomationFoundation.Runtime.Tests/Stubs/StubProcessingContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutomationFoundation.Runtime.Stubs
+{
+    public class StubProcessingContext : ProcessingContext
+    {
+        public StubProcessingContext(Guid id, IServiceScope serviceScope) : base(id, serviceScope)
+        {
+        }
+    }
+}
