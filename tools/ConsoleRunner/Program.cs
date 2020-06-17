3118af263a57035d07cb8d937cd2442357474702
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2020-06-17 17:55:05 -0400

    Refactoring.

--- tools/ConsoleRunner/Program.cs
+++ tools/ConsoleRunner/Program.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Autofac.Extensions.DependencyInjection;
 using AutomationFoundation.Hosting;
 using AutomationFoundation.Hosting.Abstractions.Builders;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ConsoleRunner
 {
@@ -13,6 +15,11 @@ namespace ConsoleRunner
             return RuntimeHost.CreateBuilder<DefaultRuntimeHostBuilder>()
                 .ConfigureServices(services =>
                 {
+                    services.AddLogging(logging =>
+                    {
+                        logging.SetMinimumLevel(LogLevel.Information);
+                        logging.AddConsole();
+                    });
                     services.AddAutofac();
                 })
                 .UseRunStrategy<CtrlCRuntimeHostRunAsyncStrategy>()
@@ -26,9 +33,8 @@ namespace ConsoleRunner
                 using var host = CreateRuntimeHostBuilder().Build();
                 await host.RunAsync(shutdownTimeoutMs: 30000);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                await Console.Error.WriteLineAsync(ex.ToString());
                 Environment.ExitCode = -1;
             }
         }
