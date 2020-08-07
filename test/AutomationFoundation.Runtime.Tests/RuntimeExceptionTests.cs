7d158f569b001c0134fedfe27c8dd5b0660fa7d5
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2020-08-07 08:18:32 -0400

    Adding unit tests.

---
+++ test/AutomationFoundation.Runtime.Tests/RuntimeExceptionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AutomationFoundation.Runtime
+{
+    [TestFixture]
+    public class RuntimeExceptionTests
+    {
+        [Test]
+        public void SetsTheMessageAsExpected1()
+        {
+            var target = new RuntimeException("This is a test message!");
+            Assert.AreEqual(target.Message, "This is a test message!");
+        }
+
+        [Test]
+        public void SetsTheMessageAndInnerExceptionAsExpected1()
+        {
+            var innerEx = new Exception("This is a test exception!");
+
+            var target = new RuntimeException("This is a test message!", innerEx);
+
+            Assert.AreEqual(target.Message, "This is a test message!");
+            Assert.AreEqual(target.InnerException, innerEx);
+        }
+    }
+}
