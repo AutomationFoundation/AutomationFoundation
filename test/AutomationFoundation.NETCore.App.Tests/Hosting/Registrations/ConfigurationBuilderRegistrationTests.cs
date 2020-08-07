bc04219c1ab7d6d0e3a2020f111740e4a94caa82
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2020-08-07 07:58:57 -0400

    Adding unit tests.

--- test/AutomationFoundation.NETCore.App.Tests/Hosting/Registrations/ConfigurationBuilderRegistrationTests.cs
+++ test/AutomationFoundation.NETCore.App.Tests/Hosting/Registrations/ConfigurationBuilderRegistrationTests.cs
@@ -11,10 +11,44 @@ namespace AutomationFoundation.NETCore.App.Hosting.Registrations
     [TestFixture]
     public class ConfigurationBuilderRegistrationTests
     {
+        private Mock<IConfigurationBuilder> builder;
+        private Mock<IServiceCollection> services;
+
+        private ConfigurationBuilderRegistration target;
+
+        [SetUp]
+        public void Init()
+        {
+            builder = new Mock<IConfigurationBuilder>();
+            services = new Mock<IServiceCollection>();
+
+            target = new ConfigurationBuilderRegistration(builder.Object, services.Object, _ => { });
+        }
+
+        [Test]
+        public void ThrowsAnExceptionWhenBuilderIsNullForConstructor()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                _ = new ConfigurationBuilderRegistration(null, services.Object, _ => { }));
+        }
+
+        [Test]
+        public void ThrowsAnExceptionWhenServicesIsNullForConstructor()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                _ = new ConfigurationBuilderRegistration(builder.Object, null, _ => { }));
+        }
+
+        [Test]
+        public void ThrowsAnExceptionWhenCallbackIsNullForConstructor()
+        {
+            Assert.Throws<ArgumentNullException>(() => _ = new ConfigurationBuilderRegistration(builder.Object, services.Object, null));
+        }
+
         [Test]
         public void ThrowsExceptionWhenServicesIsNull()
         {
-            Assert.Throws<ArgumentNullException>(() => ConfigurationBuilderRegistration.OnConfigureServicesCallback(null, builder => { }));
+            Assert.Throws<ArgumentNullException>(() => ConfigurationBuilderRegistration.OnConfigureServicesCallback(null, c => { }));
         }
 
         [Test]
@@ -27,9 +61,7 @@ namespace AutomationFoundation.NETCore.App.Hosting.Registrations
         public void ExecutesTheCallbackAsExpected()
         {
             var called = false;
-            var services = new Mock<IServiceCollection>();
-
-            ConfigurationBuilderRegistration.OnConfigureServicesCallback(services.Object, builder => { called = true; });
+            ConfigurationBuilderRegistration.OnConfigureServicesCallback(services.Object, c => { called = true; });
 
             Assert.True(called);
         }
@@ -37,11 +69,6 @@ namespace AutomationFoundation.NETCore.App.Hosting.Registrations
         [Test]
         public void ThrowsExceptionWhenConfigurationIsNull()
         {
-            var builder = new Mock<IConfigurationBuilder>();
-            var services = new Mock<IServiceCollection>();
-
-            var target = new ConfigurationBuilderRegistration(builder.Object, services.Object, _ => { });
-
             Assert.Throws<BuildException>(() => target.Register());
             builder.Verify(o => o.Build(), Times.Once);
         }
@@ -50,13 +77,8 @@ namespace AutomationFoundation.NETCore.App.Hosting.Registrations
         public void RegistersTheConfigurationAsSingleton()
         {
             var configuration = new Mock<IConfigurationRoot>();
-
-            var builder = new Mock<IConfigurationBuilder>();
             builder.Setup(o => o.Build()).Returns(configuration.Object);
 
-            var services = new Mock<IServiceCollection>();
-
-            var target = new ConfigurationBuilderRegistration(builder.Object, services.Object, _ => { });
             target.Register();
 
             builder.Verify(o => o.Build(), Times.Once);
