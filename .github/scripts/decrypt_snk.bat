f5f0e6004168d556f78d7c55d179987b26f8ae89
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2024-03-03 09:38:30 -0500

    Updates.

--- .github/scripts/decrypt_snk.bat
+++ .github/scripts/decrypt_snk.bat
@@ -1,1 +1,6 @@
+echo "Decrypting key..."
 gpg --quiet --batch --yes --passphrase %SNK_PASSPHRASE% --output Winnster.snk -d ./.github/secrets/Winnster.snk.gpg
+
+echo "Extracting public key..."
+sn -p Winnster.snk Winnster.PublicKey
+sn -tp Winnster.PublicKey
