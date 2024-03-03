3364f9b3dc128f653f11a2a64b5e3ca29312402e
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2024-03-03 10:01:21 -0500

    Updates.

--- .github/scripts/decrypt_snk.bat
+++ .github/scripts/decrypt_snk.bat
@@ -1,8 +1,1 @@
-SET sn="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe"
-
-echo "Decrypting key..."
 gpg --quiet --batch --yes --passphrase %SNK_PASSPHRASE% --output Winnster.snk -d ./.github/secrets/Winnster.snk.gpg
-
-echo "Extracting public key..."
-%sn% -p Winnster.snk Winnster.PublicKey
-%sn% -tp Winnster.PublicKey
